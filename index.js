5e8a008bbf8818de6469b19244a488a37079f6c3
Iddrejiot <162913557+Iddrejiot@users.noreply.github.com>
2024-03-10 19:39:06 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Elmés Beszélgetések dc"];
+const statusMessages = ["Nézi az Elmés Beszélgetések dc-t"];
 
 
 let currentIndex = 0;
