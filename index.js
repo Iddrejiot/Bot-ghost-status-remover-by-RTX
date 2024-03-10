f81d347fce18c1ae88b6a8c3f297bc1216493b83
Iddrejiot <162913557+Iddrejiot@users.noreply.github.com>
2024-03-10 19:21:12 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Watching Az Elmés Beszélgetések dc-t"];
 
 
 let currentIndex = 0;
